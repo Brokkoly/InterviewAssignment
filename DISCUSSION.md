3ed7c5b6292395d6d83aa56035a9da780cf072e9
Brendan Krull <brendankrull@gmail.com>
2025-11-02 15:11:37 -0600

    Add search value to url to send to server

--- DISCUSSION.md
+++ DISCUSSION.md
@@ -2,4 +2,13 @@
 - Ah dang I don't have docker set up on my home computer. I'm not going to go through that right now.
 - Time to just look at some stuff about tailwind and then get started.
 
+#### First-UI Fix
 - Feeling the rust from not using HTML and typescript in a bit, but it's coming back to me.
+
+#### Initial Server Optimization
+- Realized I could probably get docker set up quickly, so let's see.
+- Alright, getting an error message and I don't know enough docker stuff to figure that out in the time limit. I'll just pseudocode some of that for the time being.
+- Took a look at drizzle's website and I don't have time to be figuring that out since I already have the database.
+  - I'll put in what I think would be correct later in comments
+
+- Like all good server optimizations it requires UI changes
--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,66 @@
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+import { Advocate } from "@/app/page";
 
-  const data = advocateData;
+export async function GET(request: NextRequest) {
+  // Uncomment these lines if using a database.
+  //const data = await db.select().from(advocates);
+  const {searchParams} = new URL(request.url);
+
+  var start = Number.parseInt(searchParams.get("start") ?? "0");
+  var take = Number.parseInt(searchParams.get("take") ?? "-1");
+  var query = {
+    offset: start,
+    limit: take <= 0 ? null : take,
+  };
+  var searchValue = searchParams.get("searchValue") ?? "";
+
+  console.log({searchValue});
+
+  var data = advocateData as Advocate[];
+
+  if (start != 0) {
+    data = data.slice(start);
+  }
+
+  if (searchValue != "") {
+    //Real code would look similar except it would be a WHERE query with OR statements
+    //doing the same thing: checking to see if one of them includes the search term.
+    //This still feels costly
+    console.log(searchValue);
+    data = data.filter((adv: Advocate) => {
+      return AdvocateHasSearchValue(adv, searchValue);
+    });
+  }
 
   return Response.json({ data });
+}
+
+function AdvocateHasSearchValue(
+  advocate: Advocate,
+  searchTerm: string
+): boolean {
+  return (
+    advocate.firstName.includes(searchTerm) ||
+    advocate.lastName.includes(searchTerm) ||
+    advocate.city.includes(searchTerm) ||
+    advocate.degree.includes(searchTerm) ||
+    ArrayHasOneOrMoreMatches(advocate.specialties, searchTerm) ||
+    advocate.yearsOfExperience.toString().includes(searchTerm)
+  );
+}
+
+function ArrayHasOneOrMoreMatches(
+  array: string[],
+  searchTerm: string
+): boolean {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] != "" && array[i].includes(searchTerm)) {
+      return true;
+    }
+  }
+  return false;
 }
